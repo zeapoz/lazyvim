4c4f343ef249b1cea0becb88c39eba15912f494e
zeapoz <zeapo@pm.me>
2023-10-23 18:38:30 +0200

    telescope: new pickers

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -45,7 +45,49 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
-    keys = { { "<leader>.", Util.telescope("files"), desc = "Find Files (root directory)" } },
+    dependencies = {
+      {
+        "debugloop/telescope-undo.nvim",
+        keys = { { "<leader>U", "<cmd>Telescope undo<cr>" } },
+        config = function()
+          require("telescope").load_extension("undo")
+        end,
+      },
+    },
+    keys = {
+      { "<leader>.", Util.telescope("files"), desc = "Find Files (root directory)" },
+      {
+        "<leader>fP",
+        function()
+          require("telescope.builtin").find_files({
+            cwd = require("lazy.core.config").options.root,
+          })
+        end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fl",
+        function()
+          local files = {} ---@type table<string, string>
+          for _, plugin in pairs(require("lazy.core.config").plugins) do
+            repeat
+              if plugin._.module then
+                local info = vim.loader.find(plugin._.module)[1]
+                if info then
+                  files[info.modpath] = info.modpath
+                end
+              end
+              plugin = plugin._.super
+            until not plugin
+          end
+          require("telescope.builtin").live_grep({
+            default_text = "/",
+            search_dirs = vim.tbl_values(files),
+          })
+        end,
+        desc = "Find Lazy Plugin Spec",
+      },
+    },
     opts = {
       defaults = {
         layout_strategy = "flex",
