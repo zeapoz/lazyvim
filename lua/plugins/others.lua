826a1302205ace33f4de5551151e10d6a05831e9
zeapoz <zeapo@pm.me>
2024-04-25 22:06:51 +0200

    feat: smart-splits

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,10 +1,24 @@
 return {
   { "direnv/direnv.vim" },
+
   {
     "willothy/wezterm.nvim",
     config = true,
     init = function()
       require("wezterm").set_user_var("set_padding", 0)
     end,
+  },
+
+  {
+    "mrjones2014/smart-splits.nvim",
+    config = true,
+    lazy = false,
+    -- stylua: ignore
+    keys = {
+      { "<A-Left>", function() require("smart-splits").move_cursor_left() end },
+      { "<A-Down>", function() require("smart-splits").move_cursor_down() end },
+      { "<A-Up>", function() require("smart-splits").move_cursor_up() end },
+      { "<A-Right>", function() require("smart-splits").move_cursor_right() end },
+    },
   },
 }
