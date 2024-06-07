7806fc8834125409d38c599448e4129356e63e78
zeapoz <zeapo@pm.me>
2024-06-07 18:14:42 +0200

    vscode: move most keybindings to vscode config

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -32,22 +32,8 @@ end
 vim.api.nvim_create_autocmd("User", {
   pattern = "LazyVimKeymaps",
   callback = function()
-    vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
-    vim.keymap.set("n", "<leader>f", "<cmd>Find<cr>")
-    vim.keymap.set("n", "<leader>.", "<cmd>Find<cr>")
-    map_vscode("n", "<leader>e", "workbench.view.explorer")
-    map_vscode("n", "<leader>/", "workbench.action.findInFiles")
-    map_vscode("n", "<leader>s", "workbench.action.gotoSymbol")
-    map_vscode("n", "<leader>S", "workbench.action.showAllSymbols")
-    map_vscode("n", "<leader>r", "editor.action.rename")
-    map_vscode("n", "<leader>a", "editor.action.quickFix")
     map_vscode("n", "gr", "references-view.findReferences")
-    map_vscode("n", ";", "workbench.action.showCommands")
-    map_vscode("n", "]d", "editor.action.marker.nextInFiles")
-    map_vscode("n", "[d", "editor.action.marker.prevInFiles")
-    map_vscode("n", "<leader>b", "workbench.action.toggleSidebarVisibility")
-    -- Reload configuration.
-    map_vscode("n", "<leader>R", "vscode-neovim.restart")
+    map_vscode("n", "gi", "references-view.findImplementations")
   end,
 })
 
